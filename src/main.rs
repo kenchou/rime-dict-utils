712c48eaa00d68f5752c5841558145194d5f62b6
Ken Chou <zhang-j@klab.com>
2023-09-21 17:39:46 +0800

    add command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Subcommand, Parser};
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
@@ -6,37 +6,57 @@ use std::io::BufReader;
 use std::path::PathBuf;
 
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// does testing things
+    #[command(arg_required_else_help = true)]
+    Deduplicate {
+        /// Sets a custom config file
+        #[arg(short, long, value_name = "FILE")]
+        config: Option<PathBuf>,
+    },
 }
 
 
 fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
-    if let Some(path) = cli.config.as_deref() {
-        let file = File::open(path)?;
-        let buf_reader = BufReader::new(file);
-
-        let mut lines: HashSet<String> = HashSet::new();
-        let mut data_start = false;
-        for line in buf_reader.lines() {
-            let line = line?;
-            if line == "..." {
-                data_start = true;
-            } 
-            if ! data_start {   // keep origin lines before "..."
-                println!("{}", line);
-            } else if ! lines.contains(&line) {
-                println!("{}", line);
-                lines.insert(line);
-            } // else ignore duplication lines
+    match cli.command {
+        Commands::Deduplicate { config } => {
+            println!("Deduplicate {config:#?}");
+            if let Some(dict_file) = config {
+                return deduplicate(dict_file);
+            }
         }
     }
 
+    Ok(())
+}
+
+fn deduplicate(dict_file: PathBuf) -> std::io::Result<()> {
+    let file = File::open(dict_file)?;
+    let buf_reader = BufReader::new(file);
+
+    let mut lines: HashSet<String> = HashSet::new();
+    let mut data_start = false;
+    for line in buf_reader.lines() {
+        let line = line?;
+        if line == "..." {
+            data_start = true;
+        } 
+        if ! data_start {   // keep origin lines before "..."
+            println!("{}", line);
+        } else if ! lines.contains(&line) {
+            println!("{}", line);
+            lines.insert(line);
+        } // else ignore duplication lines
+    }
     Ok(())
 }
